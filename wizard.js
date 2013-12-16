f6d98e86f93dc2aacf50b54f5929479c16189224
Zac Echola <zac.echola@so.mnscu.edu>
2013-12-16 14:39:36 -0600

    Adds jQuery plugin

---
+++ wizard.js
@@ -0,0 +1,253 @@
+// Author: Zac Echola <zac.echola@so.mnscu.edu>
+
+// This plugin will progressively create a wizard ui pattern to an element,
+// allowing for easy fallback to noscript without actually using noscript markup.
+// For example, you would pass a container class such as $('.chunk-container')
+// to the plugin and it will find all headers within that container for the list, then apply
+// appropriate Bootstrap nav classes to the page, along with previous and next buttons.
+// Each page of the wizard should have a div with a "chunk" class (that's an optional name). 
+// The plugin will handle pagination from there.
+
+// Usage: 
+//  <section class='chunk-container'>
+//      <section class='chunk'>
+//          <h2>Hello</h2>
+//          ...
+//      </section>
+//      <section class='chunk'>
+//          <h2>World</h2>
+//          ...
+//      </section>
+//  </section>
+//  <script>
+//      $('.chunk-container').wizardBuilder({modifierClass: "tabs", nextOption: "default"});
+//  </script>
+//
+//  Known issues:
+//      - Doesn't play nicely if more than one wizard is on the page.
+//      - Not yet Bootstrap feature complete.
+//      - Nav doesn't like multiple headers with the same text values
+
+;(function ($, window, document, undefined) {
+    'use strict';
+
+    // Create the defaults
+    var pluginName = "wizardBuilder",
+        defaults = {
+            // Bootstrap nav specific:
+            modifierClass: "pills", // pills, tabs
+            /* currently unimplemented
+            justified: false,  // false, true
+            dropdowns: false, // false, true
+            stacked: false,  // false, true
+            */
+            
+            // Bootstrap button specific:
+            buttonSize: "", // "", lg, sm, xs
+            nextOption: "primary", // primary, default, success, info, warning, danger, link
+            previousOption: "default", // primary, default, success, info, warning, danger, link
+            // chevronGlyphicons: false, // currently unimplemented
+
+            // These are opinions, feel free to have your own
+            headerElement: "h2",
+            chunkClassName: ".chunk",
+
+            // TODO: Allow for dropdown menus, but how to display in wizard?
+
+        };
+
+    // Plugin constructor
+    function Plugin (element, options) {
+        this.element = element;
+        this.options = $.extend( {}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+    }
+
+    Plugin.prototype = {
+
+        init: function () {
+
+            // Build the UI elements
+            this.createNav(this.element);
+            this.createButtons(this.element);
+
+            // Manage the app state
+            this.state(this.element, this.options);
+
+        },
+
+        createNav: function (el) {
+            try {
+                var headerText = [],
+                    html = '';
+
+                $(this.options.headerElement).each(function() {
+                    headerText.push($(this).text());
+                });
+               
+                // TODO: Make this less dirty, concats are lazy
+                html += '<ul class="wizard-nav nav nav-' + this.options.modifierClass + '">';
+                for (var i = 0, l = headerText.length; i < l; i++) {
+                    var v = headerText[i];
+                    html += '<li><a href="#">' + v + '</a></li>';
+                }
+                html += '</ul>';
+                
+                $(el).prepend(html);
+
+            } catch (e) {
+                console.log(e);
+            }
+        },
+
+        createButtons: function (el) {
+            try {
+                var btnSize = this.options.buttonSize,
+                    nextOption = this.options.nextOption,
+                    previousOption = this.options.previousOption,
+                    html = '';
+
+                // TODO: Again with the concat!
+                html = '<div class="wizard-btn-group btn-group pull-right">';
+                html += '<button type="button" class="prev-btn btn btn-' + previousOption + '">&larr; Previous</button>';
+                html += '<button type="button" class="next-btn btn btn-' + nextOption + '">Next &rarr;</button>';
+                html += '</div>';
+
+                $(el).append(html);
+
+                // Add size class if necessary
+                if (btnSize !== "") {
+                    $('button').addClass('btn-' + btnSize);
+                }
+
+            } catch (e) {
+                console.log(e);
+            }
+        },
+
+        state: function (el, options) {
+
+            // Initial state on ready
+            if ($('.wizard-nav .active') !== []) {
+                $('.wizard-nav li').first().addClass('active');
+                $(this.options.chunkClassName + ':gt(0)').each(function() {
+                    $(this).addClass('hide');
+                });
+                $('.prev-btn').addClass('disabled');
+            }
+
+            var CurrentState = {
+                activeNav: $('.wizard-nav li.active'),
+                nextNav:  $('.wizard-nav .active').next(),
+                prevNav:  $('.wizard-nav .active').prev(),
+                activeChunk:   $(this.options.chunkClassName).not('.hide'),
+                nextChunk:  $(this.options.chunkClassName).not('.hide').next(this.options.chunkClassName),
+                prevChunk:   $(this.options.chunkClassName).not('.hide').prev(this.options.chunkClassName),
+            },
+
+
+            navButtonEnableDisable = function() {
+                var $activeNav = $('.wizard-nav .active');
+
+                if ($activeNav.next().length === 0) {
+                    $('.next-btn').addClass('disabled');
+                } else if ($activeNav.next().length !== 0) {
+                    $('.next-btn').removeClass('disabled');
+                }
+
+                if ($activeNav.prev().length === 0) {
+                    $('.prev-btn').addClass('disabled');
+                } else if ($activeNav.prev().length !== 0) {
+                    $('.prev-btn').removeClass('disabled');
+                }
+            };
+
+
+            // UI bindings
+
+            $('.wizard-btn-group').on('click', function() {
+                navButtonEnableDisable();
+            });
+            
+
+            // TODO: DRY these three bindings into a single function
+            $('.wizard-btn-group .next-btn').on('click', function() {
+
+                $(CurrentState.activeNav).removeClass('active');
+                $(CurrentState.nextNav).addClass('active');
+                
+                CurrentState.activeNav = $('.wizard-nav .active');
+                CurrentState.nextNav = CurrentState.activeNav.next();
+                CurrentState.prevNav = CurrentState.activeNav.prev();
+
+                $(CurrentState.activeChunk).addClass('hide');
+                $(CurrentState.nextChunk).removeClass('hide');
+
+                CurrentState.activeChunk = $(options.chunkClassName).not('.hide');
+                CurrentState.nextChunk = CurrentState.activeChunk.next(options.chunkClassName);
+                CurrentState.prevChunk = CurrentState.activeChunk.prev(options.chunkClassName);
+
+            });
+
+            $('.wizard-btn-group .prev-btn').on('click', function() {
+
+                $(CurrentState.activeNav).removeClass('active');
+                $(CurrentState.prevNav).addClass('active');
+                
+                CurrentState.activeNav = $('.wizard-nav .active');
+                CurrentState.nextNav = CurrentState.activeNav.next();
+                CurrentState.prevNav = CurrentState.activeNav.prev();
+
+                $(CurrentState.activeChunk).addClass('hide');
+                $(CurrentState.prevChunk).removeClass('hide');
+
+                CurrentState.activeChunk = $(options.chunkClassName).not('.hide');
+                CurrentState.nextChunk = CurrentState.activeChunk.next(options.chunkClassName);
+                CurrentState.prevChunk = CurrentState.activeChunk.prev(options.chunkClassName);
+
+            });
+
+            $('.wizard-nav a').on('click', function() {
+
+                // TODO: Make this handle multiple headerElements with the same name.
+                
+                var $clickedNav = $(this),
+                    classOption = options.chunkClassName,
+                    headerOption = options.headerElement,
+                    map = $(classOption + ' ' + headerOption + ':contains(' + $clickedNav.text() + ')'),
+                    mappedChunk = map.closest('section');
+                
+                $(CurrentState.activeNav).removeClass('active');
+                $($clickedNav).closest('li').addClass('active');
+                
+                CurrentState.activeNav = $('.wizard-nav .active');
+                CurrentState.nextNav = CurrentState.activeNav.next();
+                CurrentState.prevNav = CurrentState.activeNav.prev();
+
+                $(CurrentState.activeChunk).addClass('hide');
+                $(mappedChunk).removeClass('hide');
+
+                CurrentState.activeChunk = $(options.chunkClassName).not('.hide');
+                CurrentState.nextChunk = CurrentState.activeChunk.next(options.chunkClassName);
+                CurrentState.prevChunk = CurrentState.activeChunk.prev(options.chunkClassName);
+
+                navButtonEnableDisable();
+            });
+
+        },
+        
+    };
+
+    // Wrapper
+    $.fn[pluginName] = function (options) {
+        return this.each(function () {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName,
+                new Plugin(this, options));
+            }
+        });
+    };
+
+})(jQuery, window, document);
